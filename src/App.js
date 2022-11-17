50d1f5599e3aaa4dab0a3d7e37dd82046cd61447
GurevichV <melnikova.viki@yandex.ru>
2022-11-17 12:00:25 +0200

    ь

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Accordion from './components/Accordions';
+import Footer from './components/Footer'
+import 'accordion-js/dist/accordion.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <img src='/vdud/assets/images/banner.jpg' alt="banner"/>
+        <div>
+          <h1>Рекомендации от гостей Дудя</h1>
+          <h2>
+            Здесь собранны в основном фильмы и книги которые были упомянуты в интервью. Список не полный и будет пополнятся.
+          </h2>
+        </div>
+        <Accordion/>
+        </main>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
